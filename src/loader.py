dd339e589b424485e8229b477c7ff22aec3bf6ac
VickySekhon <sekh4498@mylaurier.ca>
2026-02-27 20:26:24 -0500

    refactor: making small changes before altering sliding window

---
+++ src/loader.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import os
+
+IMAGE_PATH = os.path.join(os.getcwd(), "data/images/")
+GT_PATH = os.path.join(os.getcwd(), "data/ground-truth/")
+OCR_PATH = os.path.join(os.getcwd(), "data/ocr-text/")
+
+def load_bln600_metadata():
+     """
+     Returns a DataFrame with metadata, which includes:
+
+     IDs (page identifiers)
+     File paths for images (strings like "data/images/3200797037.jpg")
+     Machine OCR text (the actual text content)
+     Human GT text (the actual ground truth text content)
+     """
+     file_view_obj = os.listdir(IMAGE_PATH)
+     
+     data = {
+          "page-id": [],
+          "image": [],
+          "ocr_text": [],
+          "ground_truth": []
+     }
+     
+     for filename in file_view_obj:
+          image_file_path = os.path.join(IMAGE_PATH, filename)
+          
+          page_id = filename.split(".")[0]
+          filename = page_id + ".txt" 
+          ocr_file_path = os.path.join(OCR_PATH, filename)
+          gt_file_path = os.path.join(GT_PATH, filename)
+          
+          with open(ocr_file_path, "r") as ocr, open(gt_file_path, "r") as gt:
+               ocr_text, gt_text = ocr.read(), gt.read()
+          
+          data["page-id"].append(int(page_id))
+          data["image"].append(image_file_path)
+          data["ocr_text"].append(ocr_text)
+          data["ground_truth"].append(gt_text)
+          
+     df = pd.DataFrame(data, index=data["page-id"]) # use ids to give each row of df a unique identifier
+     
+     # Summary
+     # print(f"""
+     # df:
+     # shape: {df.shape}
+     # head: {df.head()}
+     # tail: {df.tail()}
+     # size: {df.size}
+     # """)
+     
+     return df
+
+def load_text_pair(page_id):
+     df = load_bln600_metadata()
+     
+     page_entry = df[df["page-id"] == page_id]
+     if page_entry.empty:
+          print(f"Page-id: {page_id} was not found.")
+          return
+     
+     return (page_entry["ground_truth"], page_entry["ocr_text"])
+     
+
+x = load_text_pair(3200797037)
+if x:
+     print(x)
