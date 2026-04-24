cd4da364ccf8546a9dd61e28ae1e49b2d40c1f5a
VickySekhon <sekh4498@mylaurier.ca>
2026-04-24 19:35:55 -0400

    refactor: **code reorganization** moved figure generation code into 'scripts' from 'src/figures'

--- scripts/entropy_vs_cer.py
+++ scripts/entropy_vs_cer.py
@@ -9,31 +9,38 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def visualize_cer(df, top_k, indicator="avg_bits_per_token"):
+def visualize_cer(df, top_k, output, indicator="avg_bits_per_token"):
     x, y = df[f"{indicator}"], df["cer"]
+
     if indicator == "avg_surprisal_per_token":
         indicator = "Surprisal"
     else:
         indicator = "Entropy"
+
     plt.figure(figsize=(10, 6))
     ax = sns.regplot(x=x, y=y, ci=None, line_kws={"color": "red"})
+
     params = {
         "xlabel": f"{indicator}",
         "ylabel": "CER",
         "title": f"The Relationship Between {indicator} and CER For K = {top_k}",
     }
+
     ax.set(**params)
-    plt.savefig(f"figures/{indicator.lower()}_vs_cer_k_{top_k}.png")
+    plt.savefig(f"{output}/figures/{indicator.lower()}_vs_cer_k_{top_k}.png", dpi=200)
 
 
-def visualize_entropy_distribution(df, top_k):
+def visualize_entropy_distribution(df, top_k, output):
     data = df["avg_bits_per_token"]
+
     plt.figure(figsize=(10, 6))
     plt.hist(data, bins=len(df))
+
     plt.xlabel("Entropy (Average Bits Per Token)")
     plt.ylabel("Frequency of Entropy Level")
     plt.title(f"Distribution of Entropy Across Data (K = {top_k})")
-    plt.savefig(f"figures/entropy_distribution_k_{top_k}.png")
+
+    plt.savefig(f"{output}/figures/entropy_distribution_k_{top_k}.png", dpi=200)
 
 
 def main():
@@ -46,18 +53,25 @@ def main():
         type=int,
         help="How many top logprobs were considered when computing entropy",
     )
+    parser.add_argument("--output", type=str, help="Path (folder) to store output")
 
     args = parser.parse_args()
     top_k = args.top_k or 10
+    output = args.output or "results"
 
-    path_to_csv = Path(f"csvs/results_k_{top_k}.csv")
+    path_to_csv = Path(f"{output}/csv/results_k_{top_k}.csv")
+
     assert (
         path_to_csv.exists()
-    ), f"{path_to_csv} does not exist, please run `make run-all` first to generate it."
+    ), f"{path_to_csv} does not exist, please run `make run-all` first to generate the results csv file."
 
-    df = pd.read_csv(f"csvs/results_k_{top_k}.csv")
-    visualize_cer(df, top_k)
-    visualize_entropy_distribution(df, top_k)
+    try:
+        df = pd.read_csv(path_to_csv)
+        visualize_cer(df, top_k, output)
+        visualize_entropy_distribution(df, top_k, output)
+        print("Execution successful")
+    except Exception as e:
+        raise e
 
 
 if __name__ == "__main__":
