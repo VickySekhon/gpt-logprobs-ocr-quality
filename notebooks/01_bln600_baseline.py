65eadbcd7bd2b761f7c0b7dbc149d33ee4467244
VickySekhon <sekh4498@mylaurier.ca>
2026-04-28 23:07:36 -0400

    feat: clean errors existing in notebooks/ and tests/ directories

--- notebooks/01_bln600_baseline.py
+++ notebooks/01_bln600_baseline.py
@@ -3,9 +3,9 @@ import os
 import matplotlib.pyplot as plt
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from loader import *
-from normalization import normalize_text_all
-from metrics import cer
+from src.loader import *
+from src.normalization import normalize_text_all
+from src.metrics import cer
 
 PAGES_TO_LOAD = 20
 
@@ -18,7 +18,7 @@ for i in range(PAGES_TO_LOAD):
      page_id = df.iloc[i]["page-id"]
      ocr_s, gt_s = load_text_pair(page_id)
      
-     before_ocr, before_gt = ocr_s.array[0], gt_s.array[0]
+     before_ocr, before_gt = ocr_s, gt_s
      ocr, gt = normalize_text_all(before_ocr, before_gt)
      
      if not example_printed:
@@ -27,14 +27,18 @@ for i in range(PAGES_TO_LOAD):
           example_printed = True
           
      _cer = cer(ocr, gt)
-     x_page_id.append(i)
+     x_page_id.append(page_id)
      y_cer.append(_cer)
      
      print(f"Page ID: {page_id} has a CER of {_cer} between ocr and gt texts.")
 
-plt.figure(figsize=(10,6))
-plt.plot(x_page_id, y_cer, marker="o")
-plt.xlabel("Page ID")
+plt.figure(figsize=(10, 6))
+plt.scatter(range(len(y_cer)), y_cer, alpha=0.8)
+plt.plot(range(len(y_cer)), y_cer, alpha=0.4)
+plt.xticks(range(len(x_page_id)), x_page_id, rotation=45, ha="right")
 plt.ylabel("CER")
-plt.title("Plot for 20 pages loaded")
+plt.xlabel("Page ID")
+plt.ylim(0, max(y_cer) * 1.1 if y_cer else 1)
+plt.title("Character Error Rate (CER) for 20 Page Excerpts")
+plt.tight_layout()
 plt.show()
--- tests/test_entropy.py
+++ tests/test_entropy.py
@@ -1,23 +1,47 @@
 """
 Tests per-token entropy and entropy for all tokens of output in entropy.py by comparing it to results from scan2latex_entropy.py.
 """
-from utils import load_cache_json
-from entropy import token_entropies_from_logprobs
-from scan2latex_entropy import calculate_entropy
 
-def test_page_id_3207643520():
-     cache_key = "3207643520_gpt-4o_5_1"
+import os
+import numpy as np
+
+from src.utils import (
+    load_cache_json,
+    load_text_pair,
+    get_page_id_from_image,
+    get_cache_key,
+)
+from src.entropy import token_entropies_from_logprobs
+from src.scan2latex_entropy import calculate_entropy
+
+
+def test_page():
+    image_folder = os.path.join(os.getcwd(), "data/images")
+    page_id = np.array(
+        [get_page_id_from_image(image) for image in os.listdir(image_folder)]
+    )[0]
+    
+    if not page_id:
+        return
+
+    cache = load_cache_json()
+    cache_key = get_cache_key(page_id, "gpt_4o", 10, 1)
+    
+    try:
+        token_logprobs = cache[cache_key]["token_logprobs"]
+    except KeyError as e:
+        print(f"Key doesn't exist in Cache, run 'make run-all'")
+        raise e
      
-     cache = load_cache_json()
-     token_logprobs = cache[cache_key]["token_logprobs"]
-     
-     x1 = token_entropies_from_logprobs(token_logprobs)
-     x1 = (sum(x1), sum(x1)/len(x1))
-     # Positional entropies are not needed remove them
-     x2 = calculate_entropy(token_logprobs)[:-1]
-     
-     assert x1 == x2, f"Entropy does not match. entropy.py produced: {x1} whereas scan2latex_entropy.py produced {x2}"
+    x1 = token_entropies_from_logprobs(token_logprobs)
+    x1 = (sum(x1), sum(x1) / len(x1))
+    # Positional entropies are not needed remove them
+    x2 = calculate_entropy(token_logprobs, len(token_logprobs), 10)[:-1]
+
+    assert (
+        x1 == x2
+    ), f"Entropy does not match. entropy.py produced: {x1} whereas scan2latex_entropy.py produced {x2}"
+
 
 if __name__ == "__main__":
-     test_page_id_3207643520()
-     
+    test_page()
